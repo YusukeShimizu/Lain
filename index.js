c5f57924b0a459702ee1155e78f68787887feb95
YusukeShimizu <stm1051212@yahoo.co.jp>
2019-04-24 09:12:25 +0900

    bike shed

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const secp256k1 = require('bcrypto/lib/secp256k1');
 
 const port = 9735;
 
+secp256k1.verify("","30450221008195c0706fc69c357b3b85bf723a538af8c96d774bcb414afaa5e46b757118c9022006689c5a5b766a9374cbd8519216c0e1e89c3a1625c46891d8a4f91e7eba5fb501","522103c4c99ceb1694bda5f80cad4cd31a242ea71cfd5b4111a2bcb1f119000e66cf322103feedffec9cb8f4109d75311a72ff490c66624a1345a8001d20a4755168c00e8a2103ab69b1878b24b4b23d99c9ec3860b1b2744771f0f574423363a9a65890ba491b53ae")
+
 const priv = secp256k1.privateKeyGenerate();
 const stream = new NoiseStream();
-const socket = net.connect(port);
+const socket = net.connect(port,"207.180.244.165");
 stream.connect(
   socket,
   Buffer.from(priv,'hex'),
-  Buffer.from("03c9e1a4218c24e849cbf565ae33d4b8763774af23bc0df903a13dfeb2d4e008d62",'hex')
+  Buffer.from("03021c5f5f57322740e4ee6936452add19dc7ea7ccf90635f95119ab82a62ae268",'hex')
 );
 
 const testMessage = "Hello World!";
